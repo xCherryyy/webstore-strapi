687e9dbd7c181d839f37f3ac2458f22e56f2010a
Filip <filipkox3@gmail.com>
2025-05-27 17:52:27 +0200

    changed again database connection settings

--- config/database.js
+++ config/database.js
@@ -57,9 +57,16 @@ module.exports = ({ env }) => {
       connection: {
         client: "postgres",
         connection: {
-          connectionString: env("DATABASE_URL"),
-          ssl: {
-            rejectUnauthorized: false,
+          host: env("DATABASE_HOST"),
+          port: env.int("DATABASE_PORT"),
+          database: env("DATABASE_NAME"),
+          user: env("DATABASE_USERNAME"),
+          password: env("DATABASE_PASSWORD"),
+          ssl: env.bool("DATABASE_SSL", true) && {
+            rejectUnauthorized: env.bool(
+              "DATABASE_SSL_REJECT_UNAUTHORIZED",
+              false
+            ),
           },
         },
       },
